f0fefce67fe7ac7b007de977634c03e7cdc3407f
Jakub Abramczyk <jakub.abramczyk98@gmail.com>
2021-05-24 21:16:25 +0200

    Update IPhotoService.cs

--- PhotoCloud/Data/Services/Interfaces/IPhotoService.cs
+++ PhotoCloud/Data/Services/Interfaces/IPhotoService.cs
@@ -7,13 +7,14 @@ namespace PhotoCloud.Services
     public interface IPhotoService
     {
         Task AddPhoto(string userId, byte[] photo);
-        Task<ICollection<PhotoModel>> GetUsersPhotos(string userId, int page = 0);
+        Task<ICollection<PhotoModel>> GetUsersPhotos(string userId);
 
         Task<ICollection<AlbumModel>> GetUserAlbums(string userId);
 
         Task CreateNewAlbum(string userId, string albumName, List<string> photoIds);
         Task<string> GetFirstPhotoFromAlbum(string albumId);
-        Task<AlbumModel> GetAlbumPhotos(string albumId);
+        Task<ICollection<PhotoModel>> GetAlbumPhotos(string albumId);
+        Task<string> GetAlbumName(string albumId);
 
         Task AddPhotosToAlbum(string albumId, List<string> photoIds);
     }
